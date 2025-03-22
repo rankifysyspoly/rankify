2c08c46913028ddf9af428b743d5ca371212a490
rankifysyspoly <rankifysyspolygon@gmail.com>
2025-03-22 15:07:53 +0530

    register

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String, required: true, unique: true },
+//   phone: { type: String, required: true, unique: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);
